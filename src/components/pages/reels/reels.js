6a329424de353608b1bce953b1c66c344acbb855
Toan Dinh <youquand21@gmail.com>
2023-03-30 02:25:08 -0400

    add temp div to empty pages

--- src/components/pages/reels/reels.js
+++ src/components/pages/reels/reels.js
@@ -1,6 +1,9 @@
 const Reels = () => {
     return (
-        <div className='bg-neutral-900 h-screen w-full grid justify-items-center overflow-y-scroll'>
+        <div className='bg-neutral-900 h-full w-full flex justify-center overflow-y-scroll'>
+            <div className='h-full w-full flex justify-center items-center font-sans text-white text-4xl'>
+                Nothing here yet!
+            </div>
         </div>
     )
 }
