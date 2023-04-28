78c2aaacf8a32e293a8efdd1e3c02a9718e313c8
Toan Dinh <youquand21@gmail.com>
2023-04-28 02:24:24 -0400

    fixed mui tab in userPage

--- src/components/page-components/users/userTab.js
+++ src/components/page-components/users/userTab.js
@@ -2,6 +2,8 @@ import { useEffect, useRef, useState } from 'react'
 
 import { Box, Tabs, Tab } from '@mui/material'
 
+import PropTypes from 'prop-types'
+
 import useWindowSize from '../../hooks/useWindowSize'
 
 import PostsGrid from '../posts/post-profile/postsGrid'
@@ -10,10 +12,29 @@ const TabPanel = (props) => {
 	const { children, value, index, ...other } = props
 
 	return (
-		<div role='tabpanel' hidden={value !== index} {...other}>
+		<div
+			role='tabpanel'
+			hidden={value !== index}
+			id={`simple-tabpanel-${index}`}
+			aria-labelledby={`simple-tab-${index}`}
+			{...other}
+		>
 			{value === index && <Box sx={{ px: 0 }}>{children}</Box>}
 		</div>
 	)
+}
+
+TabPanel.propTypes = {
+	children: PropTypes.node,
+	index: PropTypes.number.isRequired,
+	value: PropTypes.number.isRequired
+}
+
+function a11yProps(index) {
+	return {
+		id: `simple-tab-${index}`,
+		'aria-controls': `simple-tabpanel-${index}`
+	}
 }
 
 const UserTab = ({ userId }) => {
@@ -28,7 +49,7 @@ const UserTab = ({ userId }) => {
 		setWidth(ref.current.clientWidth)
 	}, [size])
 
-	const handleChange = (newValue) => {
+	const handleChange = (event, newValue) => {
 		setValue(newValue)
 	}
 
@@ -40,20 +61,25 @@ const UserTab = ({ userId }) => {
 					onChange={handleChange}
 					TabIndicatorProps={{ style: { background: 'white' } }}
 				>
-					<Tab label={<span className='text-white'>POSTS</span>} />
-					<Tab label={<span className='text-white'>SAVED</span>} />
-					<Tab label={<span className='text-white'>TAGGED</span>} />
+					<Tab
+						label={<span className='text-white'>POSTS</span>}
+						{...a11yProps(0)}
+					/>
+					<Tab
+						label={<span className='text-white'>SAVED</span>}
+						{...a11yProps(1)}
+					/>
+					<Tab
+						label={<span className='text-white'>TAGGED</span>}
+						{...a11yProps(2)}
+					/>
 				</Tabs>
 			</Box>
 			<TabPanel value={value} index={0}>
 				<PostsGrid key={userId} userId={userId} width={width} />
 			</TabPanel>
-			<TabPanel value={value} index={1}>
-				Nothing to display!
-			</TabPanel>
-			<TabPanel value={value} index={2}>
-				Nothing to display!
-			</TabPanel>
+			<TabPanel value={value} index={1}></TabPanel>
+			<TabPanel value={value} index={2}></TabPanel>
 		</div>
 	)
 }
